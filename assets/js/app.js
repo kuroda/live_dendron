6c2365ef709fc44cbb40f036f5db6c31b61e3fee
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-07-28 22:59:17 +0900

    Install Font awesome

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,6 +2,9 @@
 // The MiniCssExtractPlugin is used to separate it out into
 // its own CSS file.
 import css from "../css/app.css"
+import "../node_modules/@fortawesome/fontawesome-free/js/fontawesome"
+import "../node_modules/@fortawesome/fontawesome-free/js/solid"
+import "../node_modules/@fortawesome/fontawesome-free/js/regular"
 
 // webpack automatically bundles all modules in your
 // entry points. Those entry points can be configured
