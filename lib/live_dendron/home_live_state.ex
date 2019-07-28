aaa8c5cd628201114257d8f0848bcd0c60c6c3aa
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-07-28 16:35:53 +0900

    Refactoring

--- lib/live_dendron/home_live_state.ex
+++ lib/live_dendron/home_live_state.ex
@@ -1,7 +1,7 @@
 defmodule LiveDendron.HomeLiveState do
   alias LiveDendron.Core
 
-  defstruct teams: [], selected_team_id: nil
+  defstruct teams: [], selected_team: nil
 
   def construct() do
     %__MODULE__{
@@ -10,56 +10,45 @@ defmodule LiveDendron.HomeLiveState do
   end
 
   def select_team(%__MODULE__{} = state, "") do
-    %{state | selected_team_id: nil}
+    %{state | selected_team: nil}
   end
 
   def select_team(%__MODULE__{} = state, id) do
     team = Enum.find(state.teams, fn t -> t.id == id end)
-    %{state | selected_team_id: team.id}
+    %{state | selected_team: team}
   end
 
-  def toggle_team_field(%__MODULE__{selected_team_id: nil} = state, _field), do: state
+  def toggle_team_field(%__MODULE__{selected_team: nil} = state, _field), do: state
 
-  def toggle_team_field(%__MODULE__{} = state, "being_edited") do
-    teams = Enum.map(state.teams, fn team ->
-      if team.id == state.selected_team_id do
-        if team.being_edited do
-          %{team | being_edited: false}
-        else
-          %{team | being_edited: true, changeset: Core.Team.changeset(team, %{})}
-        end
+  def toggle_team_field(%__MODULE__{selected_team: selected_team} = state, "being_edited") do
+    selected_team =
+      if selected_team.being_edited do
+        %{selected_team | being_edited: false}
       else
-        team
+        %{selected_team | being_edited: true, changeset: Core.Team.changeset(selected_team, %{})}
       end
-    end)
 
-    %{state | teams: teams}
+    %{state | selected_team: selected_team}
   end
 
-  def update_team_name(%__MODULE__{selected_team_id: id} = state, %{"name" => name}) do
-    team = Enum.find(state.teams, fn t -> t.id == id end)
-
-    case Core.update_team_name(team, name) do
+  def update_team_name(%__MODULE__{selected_team: selected_team} = state, %{"name" => name}) do
+    case Core.update_team_name(selected_team, name) do
       {:ok, struct} -> replace_team(state, struct)
       {:error, changeset} -> replace_team(state, changeset)
     end
   end
 
-  defp replace_team(%__MODULE__{selected_team_id: id} = state, %Core.Team{} = team) do
+  defp replace_team(%__MODULE__{selected_team: selected_team} = state, %Core.Team{} = team) do
     team = %{team | being_edited: false, changeset: nil}
 
     teams = Enum.map(state.teams, fn t ->
-      if t.id == id, do: team, else: t
+      if t.id == selected_team.id, do: team, else: t
     end)
 
-    %{state | teams: teams}
+    %{state | teams: teams, selected_team: team}
   end
 
-  defp replace_team(%__MODULE__{selected_team_id: id} = state, %Ecto.Changeset{} = cs) do
-    teams = Enum.map(state.teams, fn t ->
-      if t.id == id, do: %{t | changeset: cs}, else: t
-    end)
-
-    %{state | teams: teams}
+  defp replace_team(%__MODULE__{selected_team: selected_team} = state, %Ecto.Changeset{} = cs) do
+    %{state | selected_team: %{selected_team | changeset: cs}}
   end
 end
