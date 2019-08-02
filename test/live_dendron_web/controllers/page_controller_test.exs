6472d81b51af0f75c21fe437b914a87163103c4f
Tsutomu Kuroda <t-kuroda@oiax.jp>
2019-08-02 21:18:01 +0900

    Remove an unused test

--- test/live_dendron_web/controllers/page_controller_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule LiveDendronWeb.PageControllerTest do
-  use LiveDendronWeb.ConnCase
-
-  test "GET /", %{conn: conn} do
-    conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
-  end
-end
