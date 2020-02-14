3e9c111704286756d2aba14a1c177ffd0f2b72cd
Tsutomu Kuroda <t-kuroda@oiax.jp>
2020-02-14 23:06:16 +0900

    Bump phoenix_live_view to 0.7.1

--- mix.exs
+++ mix.exs
@@ -33,14 +33,14 @@ defmodule LiveDendron.MixProject do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.4.12"},
+      {:phoenix, "~> 1.4.13"},
       {:phoenix_pubsub, "~> 1.1"},
       {:phoenix_ecto, "~> 4.0"},
       {:ecto_sql, "~> 3.1"},
       {:postgrex, ">= 0.0.0"},
-      {:phoenix_html, "~> 2.11"},
+      {:phoenix_html, "~> 2.13"},
       {:phoenix_live_reload, "~> 1.2", only: :dev},
-      {:phoenix_live_view, "0.6.0"},
+      {:phoenix_live_view, "0.7.1"},
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"}
